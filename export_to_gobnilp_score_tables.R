a82e7c2e6b07f9223310cdb95e553547b044a947
Felix Rios <felix.leopoldo.rios@gmail.com>
2022-05-31 21:08:59 +0200

    Fixed export tables.

--- export_to_gobnilp_score_tables.R
+++ export_to_gobnilp_score_tables.R
@@ -10,15 +10,15 @@ number2binary = function(number, noBits) {
   }
 }
 
-#Bostonpar<-scoreparameters("bge",Boston)
-#itfit<-iterativeMCMC(Bostonpar, chainout=TRUE, scoreout=TRUE)
+Bostonpar<-scoreparameters("bge",Boston)
+itfit<-iterativeMCMC(Bostonpar, chainout=TRUE, scoreout=TRUE)
 
 scorefile <- "scorefile.scores"
 
 tables <- itfit$scoretable$tables
-tables[[1]]
-
 p <- length(tables)
+
+
 write(p, file = scorefile, append = FALSE)
 
 labels <- colnames(itfit$scoretable$adjacency)
@@ -28,24 +28,38 @@ adjmat <- itfit$scoretable$adjacency
 for (i in seq(p)) {
   n_plus1 <- length(tables[[i]])
   nscores <- n_plus1 * length(tables[[i]][[1]]) # the first plus1 table
-
-  print(labels[i])  
-
-
   write(paste(labels[i], nscores), file = scorefile, append = TRUE)
-  
   potparents <- adjmat[i, adjmat[i, ] == 1]
-    print(potparents)
+  
+  # going through the plus1 table
+  # First should always be included since it has no plus1 parents
+  parent_scores <- tables[[i]][[1]]
+  n_parsets <- length(parent_scores)
+  for (k in seq(n_parsets)) {
+    bitvec <- rev(number2binary(k - 1, length(potparents)))
+    str <- paste(tables[[i]][[1]][k], sum(bitvec))
     
-  # for each plus1 table. TODO: Remove the current node
-  for (j in seq(n_plus1)) {
+    parents <- names(potparents)[as.logical(bitvec)]
+    parentstr <- paste(parents, collapse = " ")
+    str <- paste(str, parentstr)
+    write(str, file = scorefile, append = TRUE)
+  }
+  
+  j = 1
+  # for each plus1 table.
+  for (node in seq(p)) {
+    # Fix index so it matches the tables.
+    # Skip the existing parents and it self
+    if(adjmat[i, node] == 1 || i ==node){
+      next
+    } 
+
     parent_scores <- tables[[i]][[j]]
     n_parsets <- length(parent_scores)
-    plus1var <- j # TODO: find the correct one.
+    plus1var <- labels[node] 
     
     for (k in seq(n_parsets)) {
-      #print(n_parents)
-      bitvec <- number2binary(k - 1, length(potparents))
+      bitvec <- rev(number2binary(k - 1, length(potparents)))
       str <- paste(tables[[i]][[j]][k], sum(bitvec) + 1, plus1var)
 
       parents <- names(potparents)[as.logical(bitvec)]
@@ -53,5 +67,7 @@ for (i in seq(p)) {
       str <- paste(str, parentstr)
       write(str, file = scorefile, append = TRUE)
     }
+    j <- j+1
   }
+  
 }
