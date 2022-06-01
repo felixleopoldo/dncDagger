bed908ef7abeb85c6b9715c8a78a52344a7b2012
Felix Rios <felix.leopoldo.rios@gmail.com>
2022-06-01 20:00:20 +0200

    Export to proper GOBNILP scores. Jacks fix.

--- export_to_gobnilp_score_tables.R
+++ export_to_gobnilp_score_tables.R
@@ -10,14 +10,25 @@ number2binary = function(number, noBits) {
   }
 }
 
+parent_table <- function(n, p) {
+  rbind(rep(NA, n), BiDAG:::parentlistnonempty(1:p, n))[, 1:p, drop = FALSE]
+}
 
-data2 <- read.delim("asia_100.dat", sep = " ", as.is = FALSE)
+#filename <- "data/myasiandata.csv"
+filename <- "asia_100.dat"
+
+data2 <- read.delim(filename, sep = " ", as.is = FALSE)
 data <- dplyr::mutate_if(data2, is.factor, ~ as.numeric(.x)) - 1
+
+#data <- read.csv(filename, check.names = FALSE)[-1,]
+#data <- data[1:200,]
 colnames(data) <- seq(0, ncol(data)-1)
 
 
 scores <- scoreparameters("bde",data, bdepar = list(chi = 1, edgepf = 1))
 itfit <- iterativeMCMC(scores, chainout=TRUE, scoreout=TRUE)
+
+print(itfit$score)
 
 scorefile <- "scorefile.scores"
 tables <- itfit$scoretable$tables
@@ -34,20 +45,23 @@ for (i in seq(p)) {
   write(paste(labels[i], nscores), file = scorefile, append = TRUE)
   potparents <- colnames(adjmat)[adjmat[i, ] == 1]
   
+  parenttable <- parent_table(p, length(potparents))
+  
   # going through the plus1 table
   # First should always be included since it has no plus1 parents
   parent_scores <- tables[[i]][[1]]
   n_parsets <- length(parent_scores)
   for (k in seq(n_parsets)) {
-    bitvec <- rev(number2binary(k - 1, length(potparents)))
-    str <- paste(tables[[i]][[1]][k], sum(bitvec))
+    parvec <- parenttable[k, ]
+    parvec <- parvec[which(!is.na(parvec))]
+    str <- paste(tables[[i]][[1]][k], length(parvec))
     
-    parents <- potparents[as.logical(bitvec)]
+    parents <- potparents[parvec]
     parentstr <- paste(parents, collapse = " ")
     str <- paste(str, parentstr)
     write(str, file = scorefile, append = TRUE)
   }
-
+  
   # The below is for the plus1 tables
   j = 1
   for (node in seq(p)) {
@@ -56,17 +70,17 @@ for (i in seq(p)) {
     if(adjmat[i, node] == 1 || i ==node){
       next
     } 
-
+    
     parent_scores <- tables[[i]][[j+1]] # get the plus1 score table
     
     n_parsets <- length(parent_scores)
     plus1var <- labels[node] 
     
     for (k in seq(n_parsets)) {
-      # get bitvector of 
-      bitvec <- rev(number2binary(k - 1, length(potparents))) 
-      str <- paste(parent_scores[k], sum(bitvec) + 1, plus1var)
-      parents <- potparents[as.logical(bitvec)]
+      parvec <- parenttable[k, ]
+      parvec <- parvec[which(!is.na(parvec))]
+      str <- paste(parent_scores[k], length(parvec) + 1, plus1var)
+      parents <- potparents[parvec]
       parentstr <- paste(parents, collapse = " ")
       str <- paste(str, parentstr)
       write(str, file = scorefile, append = TRUE)
