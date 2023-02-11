d72f24c553a06848fd020a863d0572224342462a
Felix Rios <felix.leopoldo.rios@gmail.com>
2023-02-11 08:50:07 +0100

    Shell script for running simulations in paralell.

---
+++ runsims.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+START_SEED=$1
+N_WORKERS=$2
+N_SEEDS_EACH=$3 # Seeds each
+
+for i in $(seq 0 $((N_WORKERS-1)))
+do
+    FROM=$((i * N_SEEDS_EACH + START_SEED))
+    TO=$((i * N_SEEDS_EACH + N_SEEDS_EACH -1 + START_SEED))
+    echo $FROM
+    echo $TO
+    Rscript run_seqopt.R --seeds_from $FROM --seeds_to $TO &
+
+done
